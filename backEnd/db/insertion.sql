e74384285fa4da7275c4d01d73f6c863255f9edd
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-07-21 12:57:57 +0200

    Ajout script creation DB + Insertion de donnees test

---
+++ backEnd/db/insertion.sql
@@ -0,0 +1,24 @@
+-- Insertion data test compte utilisateur et admin
+INSERT INTO ACCOUNT VALUES (0,'test','user1','akiizsan@gmail.com','user');
+INSERT INTO ACCOUNT VALUES (1,'test','admin1','akiizsan@gmail.com','admin');
+
+-- Insertion Groupe1 
+INSERT INTO GROUPE VALUES (0,'groupe1','groupe1');
+
+-- Insertion Plateforme
+INSERT INTO PLATEFORME VALUES 
+    (0,'Riot Games', 'https://www.riotgames.com/fr', null),
+    (1,'steams', 'https://store.steampowered.com/', null);
+    
+-- Insertion 
+INSERT INTO CREDENTIALS VALUES
+    (0,'xxxxxxxx', 'xxxxxxxxx', 'akiizsan@gmail.com', 1,0),
+    (1,'xxxxxxxx', 'xxxxxxxxx', 'akiizsan0@gmail.com', 1,0);
+
+INSERT INTO MEMBRE VALUES
+    (0,0),
+    (0,1);
+
+INSERT INTO LINKED VALUES
+    (0,0),
+    (0,1);
