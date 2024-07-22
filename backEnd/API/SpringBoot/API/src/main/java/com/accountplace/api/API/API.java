4627544d08c1ab4aa7f576310bf389147ecce4cc
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-07-23 01:40:42 +0200

    Creation package API + move API.java

--- backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/API/API.java
+++ backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/API/API.java
@@ -1,4 +1,4 @@
-package com.accountplace.api;
+package com.accountplace.api.API;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +16,12 @@ public class API {
     public String user() {
         return "All users";
     }
+
+    @GetMapping("/infos")
+    public String info() {
+        return "PostgreSQL 14<br>Angular 17<br>Spring Boot 14<br>Maven 4.4.4";
+    }
+
 
 
 }
