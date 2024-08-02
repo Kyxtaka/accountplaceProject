98198a5c653d297cd8cc768e8caa9f4a45821bf8
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-08-02 19:50:47 +0200

    Refactoring Mapping

--- backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/controller/data/GroupController.java
+++ backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/controller/data/GroupController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/group")
+@RequestMapping("/api/data/group")
 public class GroupController {
 
     @Autowired
